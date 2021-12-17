300e764e76dd030f1f282c969297cb65d3044ab9
akashkumar1.patel <akashkumar1.patel@ril.com>
2021-12-17 11:59:56 +0530

    WIP

---
+++ goroutine/consumerProducer/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	done := make(chan bool)
+
+	go func() {
+		//sequential
+		prod := producer()
+		consumer(prod)
+		done <- true
+	}()
+
+	go func() {
+		size := 5
+		//parallel
+		prod2 := producerParallel(size)
+		consumerParallel(prod2, size)
+		done <- true
+	}()
+
+	for i := 0; i < 2; i++ {
+		fmt.Println(<-done)
+	}
+}
+
+func producer() chan int {
+	result := make(chan int)
+
+	go func() {
+		defer close(result)
+		for i := 0; i <= 5; i++ {
+			time.Sleep(2 * time.Second)
+			result <- i
+		}
+	}()
+
+	return result
+
+}
+
+func consumer(intCh chan int) {
+	for number := range intCh {
+		fmt.Println("seq", number)
+	}
+}
+
+func producerParallel(size int) chan int {
+	result := make(chan int)
+
+	for i := 0; i <= size; i++ {
+		go func(number int) {
+			task()
+			result <- number
+		}(i)
+	}
+
+	return result
+
+}
+
+func consumerParallel(intCh chan int, size int) {
+	for i := 0; i < size; i++ {
+		fmt.Println("parallel", <-intCh)
+	}
+}
+
+func task() {
+	time.Sleep(2 * time.Second)
+}
